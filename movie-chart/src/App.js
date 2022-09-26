b5870690b821d05ca56c7074b6449aeb91522629
drum-grammer <drumgrammer@gmail.com>
2022-09-26 15:32:28 +0900

    test rerendering with state change

--- movie-chart/src/App.js
+++ movie-chart/src/App.js
@@ -1,11 +1,17 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import {useState} from "react";
 
 function App() {
+	const [counter, setValue] = useState(0);
+	const onClick = () => setValue((prev) => prev + 1);
+	console.log("call an api")
   return (
     <div>
 		<h1 className={styles.title}>Welcome Back!!</h1>
 		<Button text={"Continue"}/>
+		<button onClick={onClick}>click me</button>
+		<h1 className={styles.title}>{counter}</h1>
     </div>
   );
 }
